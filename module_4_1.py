aece581f128d469866db58c1442b744fc461059a
xdvt <xdvt1337@gmail.com>
2024-07-23 18:52:18 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
+
+fake_divide(69, 3)
+fake_divide(3, 0)
+fake_divide(49, 7)
+true_divide(15, 0)
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def divide(first, second):
+    if second == 0:
+        return print(inf)
+    else:
+        return print(first / second)
